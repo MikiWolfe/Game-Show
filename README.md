e42245b454c6acdf93541e47c2b0c1ac24f50e77
Mikayla Bruce <mkla.w.b@gmail.com>
2021-11-21 18:57:27 -0800

    displaying question and answers on page.

--- README.md
+++ README.md
@@ -34,3 +34,38 @@ function startTimer (  {...})
 - end the game 
     - clear interval function - to stop the counter from counting down 
 
+ <div>
+        <h2>What country produces the most coffee?</h2>
+        <button>Vietnam</button>
+        <button>Brazil</button> <!--answer-->
+        <button>Indonesia</button>
+        <button>Colombia</button>
+    </div>
+    <div>
+        <h2>What country consumes the most coffee on a per-person basis?</h2>
+        <button>Canada</button>
+        <button>Iceland</button>
+        <button>Denmark</button>
+        <button>Finland</button> <!--answer-->
+    </div>
+    <div>
+        <h2>What US city drinks the most coffee?</h2>
+        <button>Seattle</button>
+        <button>New York City</button> <!--answer-->
+        <button>Portland</button>
+        <button>Los Angles</button>
+    </div>
+    <div>
+        <h2>How many cups of coffee does the adverage American drink a day?</h2>
+        <button>3.7</button>
+        <button>1.5</button>
+        <button>2.6</button>
+        <button>3.1</button> <!--answer-->
+    </div>
+    <div>
+        <h2>What country consumes the most tea?</h2>
+        <button>United Kingdom</button>
+        <button>New Zealand</button>
+        <button>Turkey</button> <!--answer-->
+        <button>Ireland</button>
+    </div
--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 var timeEl =document.getElementById('timer');
 var mainEl  = document.getElementById("main");
-document.getElementById('start').addEventListener("click",time())
+document.getElementById('start').addEventListener("click",start);
 
 var startTime = 100;
+
+const questions = [
+{
+    'question' : 'What country produces the most coffee',
+    'answers' : [ 
+       
+    {'answer' : 'Vietnam', 
+    'correct': 'false'},
+
+    {'answer':'Brazil',
+    'correct': 'true'},
+
+    {'answer':'Indonesia',
+    'correct': 'false'},
+
+    {'answer':'Colombia',
+    'correct':'false'}
+]
+}
+// ,{}
+]
+;
+
 
 function time() {
 var timer = setInterval(function() {
@@ -10,7 +33,33 @@ var timer = setInterval(function() {
     timeEl.textContent = startTime + " seconds remaining";
 
 if(startTime === 0) {
-clearInterval(timer); sendMessage() = " You are out of time! "
+clearInterval(timer); sendMessage(message) = "You are out of time!"
 }
 },1000)};
 
+function start() {
+    displayQuestion(0); time();
+}
+
+function displayQuestion (questionToShow){
+    var coffee = document.createElement("div");
+    var header =document.createElement("h2");
+    var currentQuestion = questions[questionToShow];
+    header.innerHTML= currentQuestion["question"];
+    coffee.appendChild(header);
+
+
+    for (let index = 0; index < currentQuestion["answers"].length; index++) {
+        const element = currentQuestion[index];
+        var answer= document.createElement("button");
+        answer.innerHTML=currentQuestion["answers"][index]["answer"];
+        coffee.appendChild(answer);
+    }
+
+    document.getElementById('main').appendChild(coffee);
+    
+}
+;
+
+
+//add on click on to the answer that will click on to the next question
