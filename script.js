b5ddf4223e6e645346fdebf7b00f15d8314d1222
Mikayla Bruce <mkla.w.b@gmail.com>
2021-11-21 16:54:13 -0800

    styling changes and questions for quiz show put into HTML

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-var timer = 0
+var timeEl =document.querySelector('timer');
+var mainEl  = document.getElementById("main");
+document.getElementById('start').addEventListener("click",time())
+
+var startTime = 100;
+
+function time() {
+var timer = setInterval(function() {
+    startTime--;
+    timeEl.textContent = startTime + " seconds remaining";
+
+if(startTime === 0) {
+clearInterval(timer); sendMessage() = " You are out of time! "
+}
+})};
+
