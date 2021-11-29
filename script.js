dadd443f8dd43cf297368201b556c1aca08b27d5
Mikayla Bruce <mkla.w.b@gmail.com>
2021-11-28 17:06:01 -0800

    was working on the wrong file and then realized

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 var timeEl = document.getElementById("timer");
 var mainEl = document.getElementById("main");
+
 document.getElementById("start").addEventListener("click", start);
 
 var startTime = 100; //for timer
-var userInputCorrect = 0; //to store and compare user answers
 var current = 0;
+
 var questions = [
   {
     question: "What country produces the most coffee?",
     answers: [
       { answer: "Vietnam", correct: false },
-
+      
       {
         answer: "Brazil", //right answer
         correct: true,
       },
-
+      
       { answer: "Indonesia", correct: false },
-
+      
       { answer: "Colombia", correct: false },
     ],
   },
@@ -25,11 +26,11 @@ var questions = [
     question: "What country consumes the most coffee on a per-person basis?",
     answers: [
       { answer: "Canada", correct: false },
-
+      
       { answer: "Iceland", correct: false },
-
+      
       { answer: "Denmark", correct: false },
-
+      
       {
         answer: "Finland", //right answer
         correct: true,
@@ -40,14 +41,14 @@ var questions = [
     question: "What US city drinks the most coffee?",
     answers: [
       { answer: "Seattle", correct: false },
-
+      
       {
         answer: "New York City", //right answer
         correct: true,
       },
-
+      
       { answer: "Portland", correct: false },
-
+      
       { answer: "Los Angeles", correct: false },
     ],
   },
@@ -58,11 +59,11 @@ var questions = [
         answer: "3.1", //right answer
         correct: true,
       },
-
+      
       { answer: "1.5", correct: false },
-
+      
       { answer: "2.6", correct: false },
-
+      
       { answer: "3.7", correct: false },
     ],
   },
@@ -70,14 +71,14 @@ var questions = [
     question: "What country consumes the most tea?",
     answers: [
       { answer: "United Kingdom", correct: false },
-
+      
       { answer: "New Zealand", correct: false },
-
+      
       {
         answer: "Turkey", //right answer
         correct: true,
       },
-
+      
       { answer: "Ireland", correct: false },
     ],
   },
@@ -95,7 +96,7 @@ function time() {
   var timer = setInterval(function () {
     startTime--;
     timeEl.textContent = startTime + " seconds remaining";
-
+    
     if (startTime === 0) {
       clearInterval(timer);
       alert("You are out of time!");
@@ -115,14 +116,15 @@ function displayQuestion(questionToShow) {
   var currentQuestion = questions[questionToShow];
   header.innerHTML = currentQuestion["question"];
   coffee.appendChild(header);
-
+  
   for (let index = 0; index < currentQuestion["answers"].length; index++) {
     const element = currentQuestion[index];
     var answer = document.createElement("button");
     answer.innerHTML = currentQuestion["answers"][index]["answer"];
-    coffee.appendChild(answer);
+    coffee.appendChild(answer)
+    if(answer.correct){button.dataset.correct = answer.correct};
   }
-
+  
   mainEl.appendChild(coffee);
 }
 {
@@ -130,19 +132,21 @@ function displayQuestion(questionToShow) {
     mainEl.innerHTML = "";
   }
 }
-
 // event listener on click to show next question
-mainEl.addEventListener("click", function (event) {
-  if (questions(answers((correct = true)))) {
-    userInputCorrect++;
-  } else {
-    timer = -10;
-  }
+mainEl.addEventListener("click", function () 
+{
   hideCurrent();
   current++;
+  if(questions.length > currentQuestionIndex + 1){}
   displayQuestion(current);
-
-  //compare user clicks to right answers
-
-  //if correct : true, push correct to userInput.  If wrong, time is subtracted from the clock
 });
+
+function selectAnswer(e){
+  var userSelectedButton = e.target
+  var correct = userSelectedButton.dataset.correct
+setStatusClass(document.main.correct)
+Array.from(answersButtonElement.children).forEach(button =>{
+  setStatusClass(button, button.dataset.correct)
+})}
+
+
