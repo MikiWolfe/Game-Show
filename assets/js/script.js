b3cd649b3e5a5866b566e9a7ecc965eef50329b5
Mikayla Bruce <mkla.w.b@gmail.com>
2021-12-22 23:03:16 -0800

    changes to background and updated coffee icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-var timeEl = document.getElementById("timer"); 
+var timeEl = document.getElementById("timer");
 var mainEl = document.getElementById("main");
 
 document.getElementById("start").addEventListener("click", start); //starting the quiz and the timer
 
-var startTime = 50; //start time for the timer 
-var current = 0; 
+var startTime = 50; //start time for the timer
+var current = 0;
 var correctAnswers = 0;
 
 var questions = [
@@ -85,7 +85,8 @@ var questions = [
   },
 ];
 
-function hide() { // hiding the h1, div and start button 
+function hide() {
+  // hiding the h1, div and start button
   var top = document.getElementById("head");
   if (top.style.display === "none") {
     top.style.display = "block";
@@ -94,7 +95,8 @@ function hide() { // hiding the h1, div and start button
   }
 }
 
-function time() { //starting the timer with alert when user runs out of time 
+function time() {
+  //starting the timer with alert when user runs out of time
   var timer = setInterval(function () {
     startTime--;
     timeEl.textContent = startTime + " seconds remaining";
@@ -111,26 +113,28 @@ function start() {
   time();
 }
 
-function hideCurrent() { // hiding the question to show one at time 
+function hideCurrent() {
+  // hiding the question to show one at time
   mainEl.innerHTML = "";
 }
 
 //using JavaScript to create elements
 function displayQuestion(questionToShow) {
   var coffee = document.createElement("div"); //creating a div for questions and answers
-  var header = document.createElement("h2"); //creating a header for the questions 
+  var header = document.createElement("h2"); //creating a header for the questions
   var currentQuestion = questions[questionToShow];
   header.innerHTML = currentQuestion["question"];
-  coffee.appendChild(header); // appending the header to the div 
+  coffee.appendChild(header); // appending the header to the div
 
   for (let index = 0; index < currentQuestion["answers"].length; index++) {
-    const element = currentQuestion[index]; // for loop to go through object and the index 
+    const element = currentQuestion[index]; // for loop to go through object and the index
     var answer = document.createElement("button");
     answer.innerHTML = currentQuestion["answers"][index]["answer"];
     answer.value = currentQuestion["answers"][index]["correct"];
-    
-    answer.setAttribute( //onclick able to check value that user clicked right away
-      "onClick",  
+
+    answer.setAttribute(
+      //onclick able to check value that user clicked right away
+      "onClick",
       "answerLog(this.value)"
     );
     coffee.appendChild(answer);
@@ -141,7 +145,8 @@ function displayQuestion(questionToShow) {
 
 // event listener on click to show next question
 function answerLog(correct) {
-   if (correct==="true") { // Tried to not put the value in a string but for some reason would not accept Boolean here 
+  if (correct === "true") {
+    // Tried to not put the value in a string but for some reason would not accept Boolean here
     correctAnswers++;
   } else {
     startTime = startTime - 10; //wrong answer and the timer decreases by 10 sec
@@ -151,17 +156,17 @@ function answerLog(correct) {
   if (current < questions.length - 1) {
     current++;
     displayQuestion(current);
-  } else {var initals = prompt("Please enter you initals for scores");//letting users add their itials to be stored in local storage for later  
+  } else {
+    var initals = prompt("Please enter you initals for scores"); //letting users add their itials to be stored in local storage for later
 
-  if (initals != null) { //displaying how many questions the user got correct and hiding the timer if there is still time left on it. 
-    document.getElementById("main").innerHTML +=
-   "You got " + correctAnswers + " questions right!" ; 
-   timeEl.style.display ="none" 
-
-
+    if (initals != null) {
+      //displaying how many questions the user got correct and hiding the timer if there is still time left on it.
+      document.getElementById("main").innerHTML +=
+        "You got " + correctAnswers + " questions right!";
+      timeEl.style.display = "none";
+    }
   }
-
-}}
+}
 
 /* attemping to store scores in local storage. This was the only thing I was unable to accomplish. 
 
@@ -173,5 +178,21 @@ highScore.push(score);
 
 var highScore =localStorage.setItem("correctAnswers", JSON.stringify([]));   
 localStorage.getItem('correctAnswers', JSON.parse([])) 
+
+
+localStorage.setItem('highScore', highScore);
+var HighScore = localStorage.getItem('highScore');
+if (HighScore == null || HighScore == "null") {
+  HighScore = 0;
+}
+
+if (user.points > HighScore) {
+  highScore = parseInt(HighScore);
+}
+return highScore 
+
+
+
+
 
 */
